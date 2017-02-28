0e590503d0177389d487f25c4e99597f173a138b
Matt Broadstone <mbroadst@gmail.com>
2017-02-28 13:22:28 -0500

    fix(core/where3): use of massageObject is inconsistent with drivers
    DBCollection.prototype._massageObject lets you get away with
    queries which would not be possible using any other driver.

--- jstests/core/where3.js
+++ jstests/core/where3.js
@@ -6,9 +6,9 @@ t.save({returned_date: 5});
 t.save({returned_date: 6});
 
 assert.eq(1,
-          t.find(function() {
-               return this.returned_date == 5;
-           }).count(),
+          t.find({
+            $where: function() { return this.returned_date == 5; }
+          }).count(),
           "A");
 assert.eq(1, t.find({$where: "return this.returned_date == 5;"}).count(), "B");
 assert.eq(1, t.find({$where: "this.returned_date == 5;"}).count(), "C");
